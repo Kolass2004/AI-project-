73a74c708c0263319d737240d08e631fd1ebea65
Kolass Rexon <89210243+Kolass2004@users.noreply.github.com>
2023-06-07 14:27:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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";
+const API_KEY = "sk-61HDdPKtorlHrcQ8pO6mT3BlbkFJ";
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
